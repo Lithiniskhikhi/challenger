032415faee1ff563de4dee36dbf9d22af72ecac0
Lithiniskhikhi <lithiniskhikhi9@gmail.com>
2023-08-15 10:28:52 +0200

    Static

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express()
 const port = +process.env.PORT || 3000
 //static
-app.use (express.static('../static'))
+app.use (express.static('./static'))
 app.use(express.urlencoded({
       extended: false
   }),
